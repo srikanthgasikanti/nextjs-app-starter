e8d360ca1f7cc3fed90b9610aeea9b8cb8c3a61a
srikanthgasikanti <138506335+srikanthgasikanti@users.noreply.github.com>
2025-08-22 01:04:35 +0530

    Add android-app/app/src/main/java/com/example/simpleapp/data/remote/dto/UserDto.kt 1755804874978

---
+++ android-app/app/src/main/java/com/example/simpleapp/data/remote/dto/UserDto.kt
@@ -0,0 +1,14 @@
+package com.example.simpleapp.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class UserDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("username")
+    val username: String
+)
